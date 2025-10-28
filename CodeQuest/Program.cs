56556fe63d9f165794bf1d319827cf1bf6ab4fb3
johndevera7e7 <john.devera.7e7@itb.cat>
2025-10-28 13:11:06 +0100

    Meditation and Power Points system completed

--- CodeQuest/Program.cs
+++ CodeQuest/Program.cs
@@ -1,12 +1,10 @@
-﻿cmd
-using System;
+﻿using System;
 
-public class Random
-
-class Program
+public class Program
 {
-    static void Main()
+    public static void Main()
     {
+        const string AnyKeyContinue = "Press any key to continue";
         const string MenuTitle = "===== MAIN MENU - CODEQUEST =====";
         const string MenuOption1 = "1. Train your wizard";
         const string MenuOption2 = "2. Check the dungeon";
@@ -15,14 +13,18 @@ class Program
         const string MenuPrompt = "Choose an option (1-3) - (0) to exit: ";
         const string InputErrorMessage = "Invalid input. Please enter a number between 0 and 3.";
 
-        int op = 0;
+        int power, hour, totalpower = 1, op = 0;
         bool validInput;
         string WizardName;
-        Console.WriteLine("What's your name, oh destined one?")
+
+        Random rnd = new Random();
+
+        Console.WriteLine("What's your name, oh destined one?");
         WizardName = Console.ReadLine();
 
         do
         {
+            op = 0;
             Console.WriteLine(MenuTitle);
             Console.WriteLine(MenuOption1);
             Console.WriteLine(MenuOption2);
@@ -55,7 +57,36 @@ class Program
 
             if (op == 1)
             {
+                for (int i = 1; i < 6; i++)
+                {
+                    hour= rnd.Next(7,11);
+                    power = rnd.Next(11);
+                    totalpower = totalpower + power;
+                    Console.WriteLine("Dia " + i + ": " + WizardName + ", after meditating for " + hour + " hours, you gained " + power + " points of power, your power is now of " + totalpower + "!" );
+                    Console.WriteLine(AnyKeyContinue);
+                    Console.ReadLine();
+                }
 
+                if (totalpower > 45)
+                {
+                    Console.WriteLine("You gained the rank 'Arcane Master'!");
+                    Console.WriteLine("You gained the title of 'ITB-Wizard the grey'.");
+                }
+                else if (totalpower > 40)
+                {
+                    Console.WriteLine("Wow! You can summon dragons without burning the lab!");
+                    Console.WriteLine("You gained the title of 'Elarion of the embers'.");
+                }
+                else if (totalpower > 30)
+                {
+                    Console.WriteLine("You're a summoner of magic breezes");
+                    Console.WriteLine("You gained the title of 'Arka Nullpointer'.");
+                }
+                else if (totalpower > 20)
+                {
+                    Console.WriteLine("You still confuse your wand with a spoon.");
+                    Console.WriteLine("You gained the title of 'Zyn the bugged one'.");
+                }
             }
 
 
