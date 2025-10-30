3d4fb308e1c3ac0da036086b7f32eefeff3b4143
johndevera7e7 <john.devera.7e7@itb.cat>
2025-10-30 12:43:43 +0100

    Minor fixes to close issues, closes #2

--- CodeQuest/Program.cs
+++ CodeQuest/Program.cs
@@ -22,7 +22,7 @@ public class Program
 
         Console.WriteLine("What's your name, oh destined one?");
         WizardName = Console.ReadLine();
-
+         
         do
         {
             op = 0;
